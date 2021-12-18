3958de6e364dd19cc2e4439075898551ddd86bd2
cat3582 <43181466+3582@users.noreply.github.com>
2021-12-18 00:21:55 +0000

    abc083_B

--- atcoder/abc083_B.py
+++ atcoder/abc083_B.py
@@ -1,14 +1,21 @@
-import math
+# import math
 
-N, A, B = map(int, input().split())
-x=0
-re=0
-for num in range(N):
-    ans=0
-    while num > 0:
-        ans += num  % 10
-        print(num)
-        num = math.floor(num/10)
-    if A<ans<B:
-        re += ans
-print(re)
+# N, A, B = map(int, input().split())
+# x=0
+# re=0
+# for num in range(N +1):
+#     ans=0
+#     numans=num
+#     while num > 0:
+#         ans += num % 10
+#         num = math.floor(num/10)
+#     if A<=ans<=B:
+#         re += numans
+# print(re)
+
+a,b,c = map(int,input().split())
+x = 0
+for n in range(1,a+1):
+    s = sum(list(map(int,list(str(n)))))
+    if b <= s <= c: x += n
+print(x)
